5955e50714bb54f64ad6c52b2e6fa646d9e5e99f
Elaine Thai <ethai1827@gmail.com>
2019-05-14 22:06:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var copy = object; 
+  delete copy[key];
+  return copy;
+}
